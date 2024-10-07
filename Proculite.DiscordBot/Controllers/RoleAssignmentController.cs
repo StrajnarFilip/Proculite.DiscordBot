48577ed0567c5047b9a115d7e69e1448517a2775
Filip Strajnar <filip.strajnar@gmail.com>
2024-10-07 23:10:58 +0200

    Created Controller endpoints and Views for adding role assignment messages.

--- Proculite.DiscordBot/Controllers/RoleAssignmentController.cs
+++ Proculite.DiscordBot/Controllers/RoleAssignmentController.cs
@@ -1,19 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Proculite.DiscordBot.Services;
 
 namespace Proculite.DiscordBot.Controllers
 {
     public class RoleAssignmentController : Controller
     {
         private readonly ILogger<RoleAssignmentController> _logger;
+        private readonly RoleAssignmentService _roleAssignmentService;
 
-        public RoleAssignmentController(ILogger<RoleAssignmentController> logger)
+        public RoleAssignmentController(
+            ILogger<RoleAssignmentController> logger,
+            RoleAssignmentService roleAssignmentService
+        )
         {
             _logger = logger;
+            this._roleAssignmentService = roleAssignmentService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AddRoleAssignment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMessageByLink(
+            [FromForm] string messageLink,
+            [FromForm] bool onlyOne
+        )
+        {
+            await this._roleAssignmentService.AddRoleAssignmentMessageByMessageLink(
+                messageLink,
+                onlyOne
+            );
+            return RedirectToAction("Index");
         }
     }
 }
