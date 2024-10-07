19b85c42980ccf64db8f4e61280137d358849e6b
Filip Strajnar <filip.strajnar@gmail.com>
2024-10-07 22:06:32 +0200

    Cranged DB Context to singleton.

--- Proculite.DiscordBot/Program.cs
+++ Proculite.DiscordBot/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<DiscordBotContext>();
+builder.Services.AddSingleton<DiscordBotContext>();
 builder.Services.AddSingleton<DiscordService>();
 builder.Services.AddSingleton<RoleAssignmentService>();
 
